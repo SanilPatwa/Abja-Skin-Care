3ac0f67583a8828a3d2876dedfbc4513a950296b
Sanil <patwasanil39@gmail.com>
2026-07-31 08:10:50 +0530

    cleared routes

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const pool = require("./db"); 
 const app = express();
-app.use(express.json())
+const cors = require("cors");
+app.use(express.json());
+app.use(cors());
 
 pool.query(`CREATE TABLE IF NOT EXISTS clients (
     id SERIAL PRIMARY KEY,
@@ -39,6 +41,36 @@ app.post("/api/clients",async(req,res)=>{
         console.error(err);
         res.status(500).json({error:"Database error"})
     }
+})
+
+app.delete("/api/clients/:id",async(req,res)=>{
+  try{
+    const id = req.params.id;
+    const deleteEntry = await pool.query(`DELETE FROM clients WHERE id = $1 RETURNING *`,[id])
+    if(deleteEntry.rowCount ===0){
+      return res.status(404).json({error:"Client not found "})
+    }
+    res.json({message:"Client deleted",clients:deleteEntry.rows[0]})
+  }catch(err){
+    res.status(500).json({error:"Database error"})
+  }
+  
+})
+
+app.put("/api/clients/:id",async(req,res)=>{
+  const clientsId = req.params.id;
+  const {name,type,phone,city} = req.body;
+  try{
+    const updateEntry = await pool.query("UPDATE clients SET name = $1, type = $2, phone = $3, city = $4 WHERE id = $5 RETURNING *",[name, type, phone, city, clientsId]);
+    if(updateEntry.rowCount === 0){
+      return res.status(404).json({
+        message:"Client not found"
+      })
+    }
+    res.json(updateEntry.rows[0])
+  }catch(err){
+    res.status(500).json({error:"error"})
+  }
 })
 
 app.listen(3000,()=>{
